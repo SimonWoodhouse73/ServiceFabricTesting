66545c856a207440c10196ea4566bc14c1cbd5ad
Simon Woodhouse <simonwoodhouse1@outlook.com>
2018-07-02 10:42:58 +0100

    Create DomainResources.cs

---
+++ Domain.Insolvencies/Resources/DomainResources.cs
@@ -0,0 +1,35 @@
+namespace Callcredit.Domain.Insolvencies.Resources
+{
+    public static class DomainResources
+    {
+        public const string InsolvencyOrder = "InsolvencyOrder";
+        public const string InsolvencyOrder_camelCase = "insolvencyOrder";
+        public const string InsolvencyOrders = "InsolvencyOrders";
+        public const string InsolvencyOrders_camelCase = "insolvencyOrders";
+
+        public const string Dispute = "Dispute";
+        public const string Dispute_camelCase = "dispute";
+        public const string Disputes = "Disputes";
+        public const string Disputes_camelCase = "disputes";
+
+        public const string InsolvencyOrderAddress = "InsolvencyOrderAddress";
+        public const string InsolvencyOrderAddress_camelCase = "insolvencyOrderAddress";
+        public const string InsolvencyOrderAddresses = "InsolvencyOrderAddresses";
+        public const string InsolvencyOrderAddresses_camelCase = "insolvencyOrderAddresses";
+
+        public const string InsolvencyOrderHistory = "InsolvencyOrderHistory";
+        public const string InsolvencyOrderHistory_camelCase = "insolvencyOrderHistory";
+        public const string InsolvencyOrderHistories = "InsolvencyOrderHistories";
+        public const string InsolvencyOrderHistories_camelCase = "insolvencyOrderHistories";
+
+        public const string InsolvencyOrderPerson = "InsolvencyOrderPerson";
+        public const string InsolvencyOrderPerson_camelCase = "insolvencyOrderPerson";
+        public const string InsolvencyOrderPersons = "InsolvencyOrderPersons";
+        public const string InsolvencyOrderPersons_camelCase = "insolvencyOrderPersons";
+
+        public const string InsolvencyOrderTradingDetail = "InsolvencyOrderTradingDetail";
+        public const string InsolvencyOrderTradingDetail_camelCase = "insolvencyOrderTradingDetail";
+        public const string InsolvencyOrderTradingDetails = "InsolvencyOrderTradingDetails";
+        public const string InsolvencyOrderTradingDetails_camelCase = "insolvencyOrderTradingDetails";
+    }
+}
