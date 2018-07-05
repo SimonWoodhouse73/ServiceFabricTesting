a8748aa44b9121e8fa9c7fbb399347eae4021360
Simon Woodhouse <simonwoodhouse1@outlook.com>
2018-07-05 10:26:30 +0100

    Create CertOptions.cs

---
+++ Packages/Auth.Jwt.OnPremise/Config/CertOptions.cs
@@ -0,0 +1,11 @@
+namespace Callcredit.Authorization.OnPremise.Configuration
+{
+    public class CertificateOptions
+    {
+        public string StoreName { get; set; }
+
+        public string StoreLocation { get; set; }
+
+        public string Thumbprint { get; set; }
+    }
+}
