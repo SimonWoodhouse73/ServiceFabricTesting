fbddb9fdae2dd5013c0d9b0e7552074cdd847658
Simon Woodhouse <simonwoodhouse1@outlook.com>
2018-07-05 10:21:29 +0100

    Create ExceptionMessages.cs

---
+++ Packages/AspnetCore.ProblemJSon/ExceptionMessages.cs
@@ -0,0 +1,13 @@
+namespace Callcredit.AspNetCore.ProblemJson
+{
+    /// <summary>
+    /// Contains exception messages for problems.
+    /// </summary>
+    public static class ExceptionMessages
+    {
+        /// <summary>
+        /// The string representing the Status Code Not Mapped To Exception message.
+        /// </summary>
+        public const string StatusCodeNotMappedToException = "There is no status code mapped to the exception.";
+    }
+}
